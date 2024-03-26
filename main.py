da2d9979d731b58220816612cfc6fde0b26ed08b
kalau8 <72884870+kalau8@users.noreply.github.com>
2024-03-26 15:59:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,171 +1,68 @@
-import mysql.connector
-from mysql.connector import Error
-from getpass import getpass
+from flask import Flask, render_template, request, redirect, url_for, session
+import pymysql
 
-# Function to connect the database
-def connecter():
-    try:
-        connection = mysql.connector.connect(
-                host='localhost',
-                user='root',
-                password='root1234',
-                database='futebol'
-            )
-        return connection
-    except Error as e:
-        print(f"Error to connection to MySQL: {e}")
-        return None
+app = Flask(__name__)
 
-# Function to register with the username and the password
-def register(connection):
-    username = input("\nInsert the username: ")
-    password = getpass("Insert the password: ")
-    permissions = input("\nAdmin or read?")
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO futebol.users (username, password, permissions) VALUES (%s, %s, %s)", (username, password, permissions))
-    print("\nRegister!")
+# Configurações do MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root1234'
+app.config['MYSQL_DB'] = 'futebol'
 
-    connection.commit()
-    cursor.close()
+# Função para conectar ao MySQL
+def conectar_mysql():
+    return pymysql.connect(
+        host=app.config['MYSQL_HOST'],
+        user=app.config['MYSQL_USER'],
+        password=app.config['MYSQL_PASSWORD'],
+        db=app.config['MYSQL_DB']
+    )
 
-#login with the username and the password
-def login(connection):
-    username = input("\nInsert the username: ")
-    password = getpass("Insert the password: ")
+# Rota para página inicial
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM futebol.users WHERE username = %s AND password = %s", (username, password))
-    user = cursor.fetchone()
+# Rota para a página de login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        connection = conectar_mysql()
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM futebol.users WHERE username = %s AND password = %s', (username, password))
+        user = cursor.fetchone()
+        cursor.close()  # Fecha o cursor
+        connection.close()  #
+        if user:
+            session['username'] = username
+            return redirect(url_for('dashboard'))
+        else:
+            return 'Login inválido'
+    return render_template('login.html')
 
-    if user:
-        print("\nLogin successful!")
-    else:
-        print("\nUsername or password were incorrect.")
+# Rota para a página do painel de controle
+@app.route('/dashboard')
+def dashboard():
+    if 'username' in session:
+            # Estabelece uma conexão com o banco de dados
+        connection = conectar_mysql()
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM futebol.atletas')
+        atletas = cursor.fetchall()
+        print(atletas)
+        cursor.close()  # Fecha o cursor
+        connection.close()  # Fecha a conexão com o banco de dados
+        return render_template('dashboard.html', atletas=atletas)
+    return redirect(url_for('login'))
 
-    cursor.close()
+# Rota para fazer logout
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
-# Connection to the database MySQL
-connection = connecter()
-
-if connection:
-
-    while True:
-
-        #Main Menu
-        print("\n1. Register new user")
-        print("2. Login")
-        print("3. Exit\n")
-
-        opcao = input("Choose a option: ")
-
-        #if to put 1 to check the register
-        if opcao == '1':
-            register(connection)
-        #if to put 2 to check the login and then access to the menu
-        elif opcao == '2':
-            login(connection)
-        
-            while True:
-                
-                question = input('\nWhat is the club? ')
-
-                cursor = connection.cursor()
-                cursor.execute("select * from futebol.atletas where atletas.clube='"+question+"'")
-                myresult = cursor.fetchall()
-
-                if myresult:
-
-                    question1 = input('\nWhat is the level? ')
-                    
-                    if question1=='senior':
-
-                        question2 = input('\nDo you want to see/add or remove athletes?? ')
-                        
-                        if question2=='see':
-                            cursor = connection.cursor()
-                            cursor.execute("select atletas.nome from futebol.atletas where atletas.clube='"+question+"'")
-                            myresult = cursor.fetchall()
-                            print(myresult)
-
-                            while True:
-
-                                question8 = input('\nDo you want to continue? ')
-                                
-                                if question8=='yes':
-                                    break
-                                
-                                elif question8=='no':
-                                    print('Thank you!')
-                                    exit()
-                                
-                                else:
-                                    print("Write yes or no!")
-                        
-                        elif question2=='add':
-                            question3 = input('\nWhat is the athlete\'s name? ')
-                            question4 = input('How old is the athlete?')
-                            question5 = input('What is the athlete\'s weight? ')
-                            question6 = input('How tall is the athlete? ')  
-                            cursor = connection.cursor()
-                            cursor.execute("insert into futebol.atletas(nome,idade,peso,altura,clube) values ('"+question3+"',"+question4+","+question5+",'"+question6+"','"+question+"')")
-                            myresult = cursor.fetchall()
-                            connection.commit()
-                            
-                            while True:
-
-                                question7 = input('\nQueres adicionar mais algum atleta? ')
-                            
-                                if question7=='yes':
-                                    question3 = input('\nWhat is the athlete\'s name? ')
-                                    question4 = input('How old is the athlete?')
-                                    question5 = input('What is the athlete\'s weight? ')
-                                    question6 = input('How tall is the athlete? ')        
-                                    cursor = connection.cursor()
-                                    cursor.execute("insert into futebol.atletas(nome,idade,peso,altura,clube) values ('"+question3+"',"+question4+","+question5+",'"+question6+"','"+question+"')")
-                                    myresult = cursor.fetchall()
-                                    connection.commit()
-                                    continue
-
-                                elif question7=='no':
-                                    print('Thank you!')
-                                    exit()
-                                else:
-                                    print("Write yes or no!")
-                            
-                        elif question2=='remove':
-
-                            question9 = input('\nWhich athlete\'s name would you like to delete? ')
-
-                            cursor = connection.cursor()
-                            cursor.execute("delete from futebol.atletas where atletas.nome='"+question9+"'")
-                            myresult = cursor.fetchall()
-                            connection.commit()
-                            
-                            while True:
-                        
-                                question10 = input('\nDo you want to delete any more athletes? ')
-                            
-                                if question10=='yes':
-                                    question11 = input('\nWhich athlete\'s name would you like to delete? ')
-                                    cursor = connection.cursor()
-                                    cursor.execute("delete from futebol.atletas where atletas.nome='"+question11+"'")
-                                    myresult = cursor.fetchall()
-                                    connection.commit()
-                                    continue
-
-                                elif question10=='no':
-                                    print('Thank you!')
-                                    exit()
-                                
-                                else:
-                                    print("Write yes or no!")
-                    
-                    else:
-                        print("Write the level correctly!")
-                    
-                else:
-                    print("Write the club name correctly!")
-
-        elif opcao == '3':
-            print("Goodbye my friend!")
-            exit()
+if __name__ == '__main__':
+    app.secret_key = '1234'
+    app.run(debug=True)
