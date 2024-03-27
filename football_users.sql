a08b2eea6e925959be2bf1a90a5cc240b0c6bfd5
claudioo.andre <72884870+claudiooandre@users.noreply.github.com>
2024-03-27 10:27:55 +0000

    Update football_users.sql

--- football_users.sql
+++ football_users.sql
@@ -29,7 +29,7 @@ CREATE TABLE `users` (
   `permissions` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
