a482d6d8e98bb5050082afa910f8daeaf5d30aca
claudioo.andre <72884870+claudiooandre@users.noreply.github.com>
2024-03-27 10:21:45 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import pymysql
 
 app = Flask(__name__)
 
-# Configurações do MySQL
+# Config. to database mysql
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root1234'
-app.config['MYSQL_DB'] = 'futebol'
+app.config['MYSQL_DB'] = 'football'
 
-# Função para conectar ao MySQL
+# Function to connect the database mysql
 def conectar_mysql():
     return pymysql.connect(
         host=app.config['MYSQL_HOST'],
@@ -18,12 +18,12 @@ def conectar_mysql():
         db=app.config['MYSQL_DB']
     )
 
-# Rota para página inicial
+# Route to index page
 @app.route('/')
-def index():
-    return render_template('index.html')
+def homepage():
+    return render_template('homepage.html')
 
-# Rota para a página de login
+# Route to login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -31,37 +31,38 @@ def login():
         password = request.form['password']
         connection = conectar_mysql()
         cursor = connection.cursor()
-        cursor.execute('SELECT * FROM futebol.users WHERE username = %s AND password = %s', (username, password))
+        cursor.execute('SELECT * FROM football.users WHERE username = %s AND password = %s', (username, password))
         user = cursor.fetchone()
-        cursor.close()  # Fecha o cursor
-        connection.close()  #
+        cursor.close()
+        connection.close() 
         if user:
             session['username'] = username
-            return redirect(url_for('dashboard'))
+            return redirect(url_for('main'))
         else:
-            return 'Login inválido'
+            return 'Login invalid'
     return render_template('login.html')
 
-# Rota para a página do painel de controle
-@app.route('/dashboard')
-def dashboard():
+# Route to main page
+@app.route('/main')
+def main():
     if 'username' in session:
-            # Estabelece uma conexão com o banco de dados
+        # Connection to the database
         connection = conectar_mysql()
         cursor = connection.cursor()
-        cursor.execute('SELECT * FROM futebol.atletas')
-        atletas = cursor.fetchall()
-        print(atletas)
-        cursor.close()  # Fecha o cursor
-        connection.close()  # Fecha a conexão com o banco de dados
-        return render_template('dashboard.html', atletas=atletas)
+        cursor.execute('SELECT * FROM football.athletes')
+        athletes = cursor.fetchall()
+        ###print(athletes)
+        cursor.close()
+        connection.close() 
+        return render_template('main.html', athletes=athletes)
     return redirect(url_for('login'))
 
-# Rota para fazer logout
+# Route to logout
 @app.route('/logout')
 def logout():
     session.pop('username', None)
-    return redirect(url_for('index'))
+    return redirect(url_for('homepage'))
 
 if __name__ == '__main__':
+    app.secret_key = '1234'
     app.run(debug=True)
